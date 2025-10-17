f0d42d738b50954d29e9bb23bfd1450a5fd415a6
medeiroscoding <eramarketing2009@gmail.com>
2025-10-17 16:50:24 -0300

    Add files via upload

---
+++ 06. Media Ponderada/06. Media Ponderada/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+Console.WriteLine("Calcular Media Ponderada");
+double n1, p1;
+double n2, p2;
+double n3, p3;
+
+Console.WriteLine("Digite a nota 1:");
+n1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o peso 1:");
+p1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a nota 1:");
+n2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o peso 1:");
+p2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a nota 1:");
+n3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o peso 1:");
+p3 = Convert.ToDouble(Console.ReadLine());
+
+double media;
+media = ((n1 * p1) + (n2 * p2) + (n3 * p3)) / (p1 + p2 + p3);
+Console.WriteLine($"Sua media foi: {media}");
+
+if (media >= 7.0) 
+{
+    Console.WriteLine("Aprovado");
+}
+
+else if (media >= 5.0) 
+{
+    Console.WriteLine("Recuperação");
+}
+else
+{
+    Console.WriteLine("Recuperação");
+}
+
