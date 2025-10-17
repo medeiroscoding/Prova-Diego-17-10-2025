3c36d9f339fbc9fba2f84bbf982ffaa6454bdd19
medeiroscoding <eramarketing2009@gmail.com>
2025-10-17 16:44:46 -0300

    Add files via upload

---
+++ 09. Nome/nomes/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] nomes = { "Marcos", "Odete", "Helena", "Pedro", "Maria" };
+string nomeBusca;
+int i;
+Console.WriteLine("Digite um nome para pesquisar na lista:");
+nomeBusca = Console.ReadLine();
+
+int encontrado = 0;
+for (i = 0; i < nomes.Length; i++)
+{
+    if (nomes[i] == (nomeBusca))
+    {
+        Console.WriteLine($"O nome {nomes[i]} foi encontrado na lista");
+        encontrado = 1;
+            break;
+    }
+}
+if(encontrado == 0)
+{
+    Console.WriteLine($"O nome não foi encontrado na lista");
+}
