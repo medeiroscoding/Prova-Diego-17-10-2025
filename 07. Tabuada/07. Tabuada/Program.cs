b176ab1c921ce17495170ac831a576a641e89c2f
medeiroscoding <eramarketing2009@gmail.com>
2025-10-17 16:49:35 -0300

    Add files via upload

---
+++ 07. Tabuada/07. Tabuada/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int inicio, fim;
+Console.WriteLine("Digite o numero de inicio");
+inicio = int.Parse(Console.ReadLine());
+ 
+Console.WriteLine("Digite o numero final");
+fim = int.Parse(Console.ReadLine());
+
+for (int i = inicio; i <= fim; i++)
+{
+    Console.WriteLine($"Tabuada do {i}:");
+    for (int n = 1; n <= 10; n++)
+    {
+        Console.WriteLine($"{i} x {n} = {i * n}");
+    }
+
+}
